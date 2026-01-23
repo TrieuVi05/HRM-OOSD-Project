f0cfce2612ca5e37c1e6bd5595c5845147a6918c
lungochuyennhungg <nhunglnh0391@ut.edu.vn>
2026-01-24 02:07:53 +0700

    Update Role.java

--- backend/src/main/java/com/hrm/HRM/entity/Role.java
+++ backend/src/main/java/com/hrm/HRM/entity/Role.java
@@ -1,1 +1,19 @@
+package com.hrm.HRM.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+}
 
