4be98b3506d63e78069a43721f767578bd99a162
lungochuyennhungg <nhunglnh0391@ut.edu.vn>
2026-01-24 00:10:35 +0700

    Update Departmen.java

--- backend/src/main/java/com/hrm/HRM/entity/Departmen.java
+++ backend/src/main/java/com/hrm/HRM/entity/Departmen.java
@@ -1,1 +1,18 @@
+package com.hrm.HRM.entity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "departments")
+@Getter @Setter
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+}
+
 
